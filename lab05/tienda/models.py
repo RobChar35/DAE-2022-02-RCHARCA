48ac0934af51d15043cbe2f1b22be1bfe91f5f86
Robert Charca <trevoraobac@gmail.com>
2022-09-28 15:40:02 -0500

    Django project for Lab05 created.

---
+++ lab05/tienda/models.py
@@ -0,0 +1,8 @@
+from statistics import mode
+from django.db import models
+
+# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
